1441f1b56375d574d80d9779294774dae6df0968
Babar <babarnadeem996@gmail.com>
2026-07-24 17:36:51 +0500

    fraction class created

---
+++ frac.py
@@ -0,0 +1,37 @@
+class Fraction:
+
+    def __init__(self, n, d):
+        self.num = n
+        self.den = d
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
+
+    def __add__(self, other):
+        num = self.num * other.den + other.num * self.den
+        den = self.den * other.den 
+        return Fraction(num,den)
+    
+    def __sub__(self, other):
+        num = self.num * other.den - other.num * self.den
+        den = self.den * other.den 
+        return Fraction(num,den)
+
+    def __mul__(self, other):
+            num = self.num * other.num
+            den = self.den * other.den 
+            return Fraction(num,den)
+
+    def __truediv__(self, other):
+            num = self.num * other.den 
+            den = self.den * other.num 
+            return Fraction(num,den)
+    
+
+x = Fraction(3,4)
+y = Fraction(5, 3)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
