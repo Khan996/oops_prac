8125ce8707044c2cdfd80b94e9724983525837b3
Babar <babarnadeem996@gmail.com>
2026-07-25 14:30:35 +0500

    constructor in child class

--- inheritance.py
+++ inheritance.py
@@ -39,22 +39,60 @@
 
 # ---------------------------
 # Overriding example 
-class Phone:
-    def __init__(self, price, brand, camera):
-        print("Inside Phone Constructor")
-        self.price = price
-        self.brand = brand 
-        self.camera = camera 
+# class Phone:
+#     def __init__(self, price, brand, camera):
+#         print("Inside Phone Constructor")
+#         self.price = price
+#         self.brand = brand 
+#         self.camera = camera 
 
-    def buy(self):
-        print("Buy the phone")
-class SmartPhone(Phone):
+#     def buy(self):
+#         print("Buy the phone")
+# class SmartPhone(Phone):
 
-    def buy(self):
-        print("Buy the smartphone")
+#     def buy(self):
+#         print("Buy the smartphone")
 
-s = SmartPhone(50000, "Iphone", "14")
-print(s.brand)
-print(s.price)
-print(s.camera)
-s.buy()
+# s = SmartPhone(50000, "Iphone", "14")
+# print(s.brand)
+# print(s.price)
+# print(s.camera)
+# s.buy()
+#--------------------------
+# Child-Parent Example
+# class Parent:
+
+#     def __init__(self, num):
+#         self.num = num
+
+#     def get_num(self):
+#         return self.num
+    
+# class Child(Parent):
+
+#     def show(self):
+#         print("This is in child class")
+    
+# c = Child(100)
+# print(c.get_num())
+# c.show()
+#-------------------------------------------
+# Child-Parent Example
+class Parent:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def get_num(self):
+        return self.__num
+    
+class Child(Parent):
+
+    def __init__(self, val, num):
+        self.__val = val
+
+    def get_val(self):
+        return self.__val
+    
+c = Child(100, 10)
+print(c.get_num())
