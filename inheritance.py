5dcabaa534322233745707c5bef4bcc89a57a528
Babar <babarnadeem996@gmail.com>
2026-07-25 14:19:38 +0500

    Method overriding

--- inheritance.py
+++ inheritance.py
@@ -1,22 +1,60 @@
-# For basic understanding
-class User:
+# # For basic understanding
+# class User:
 
-    def login(self):
-        return "Login"
+#     def login(self):
+#         return "Login"
 
-    def register(self):
-        return "register"
+#     def register(self):
+#         return "register"
 
-class Student(User):
+# class Student(User):
 
-    def enroll(self):
-        return "Enroll"
+#     def enroll(self):
+#         return "Enroll"
 
-    def review(self):
-        return "review"
+#     def review(self):
+#         return "review"
 
-stu1 = Student()
-print(stu1.login())
-print(stu1.register())
-print(stu1.enroll())
-print(stu1.review())
+# stu1 = Student()
+# print(stu1.login())
+# print(stu1.register())
+# print(stu1.enroll())
+# print(stu1.review())
+#-----------------------------------
+# Another basic example 
+# class Phone:
+#     def __init__(self, price, brand, camera):
+#         print("Inside Phone Constructor")
+#         self.price = price
+#         self.brand = brand 
+#         self.camera = camera 
+
+# class SmartPhone(Phone):
+#     pass
+
+# s = SmartPhone(50000, "Iphone", "14")
+# print(s.brand)
+# print(s.price)
+# print(s.camera)
+
+# ---------------------------
+# Overriding example 
+class Phone:
+    def __init__(self, price, brand, camera):
+        print("Inside Phone Constructor")
+        self.price = price
+        self.brand = brand 
+        self.camera = camera 
+
+    def buy(self):
+        print("Buy the phone")
+class SmartPhone(Phone):
+
+    def buy(self):
+        print("Buy the smartphone")
+
+s = SmartPhone(50000, "Iphone", "14")
+print(s.brand)
+print(s.price)
+print(s.camera)
+s.buy()
